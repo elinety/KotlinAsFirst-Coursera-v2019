92669ea806a9da1825c2f3060189dfdb22a87723
elinety <alena0702@gmail.com>
2020-01-27 00:20:20 +0400

    thirdDigit function

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -9,7 +9,7 @@ import kotlin.math.*
  *
  * Вычисление квадрата целого числа
  */
-fun sqr(x: Int) = x * x
+
 
 /**
  * Пример
@@ -48,9 +48,23 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
 /**
  * Пример главной функции
  */
-fun main() {
-    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-    println("Root product: $x1x2")
+fun main(args: Array<String>) {
+//    val x = 3801
+//    val result = thirdDigit(x)
+//    println(result)
+    printString("Hello, world!")
+    printString("Привет, мир")
+}
+
+fun thirdDigit(number: Int) = number / 100 % 10
+
+fun sqr(x: Int) = x * x
+
+fun printString(string: String) {
+    println("string start")
+    println(string)
+    println("string end")
+
 }
 
 /**
@@ -92,7 +106,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+//fun thirdDigit(number: Int): Int = TODO()
 
 /**
  * Простая
